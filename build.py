bcdb69db878b933a54525d89a334904b3c5f32aa
nichind <nichinddev@gmail.com>
2024-10-22 22:37:40 +0300

    fix build on linux

--- build.py
+++ build.py
@@ -7,22 +7,27 @@ import PyInstaller.__main__
 def main():
     
     add_data = []
-    locales = listdir('locales')
+    venv_path = getcwd() + '/.venv'
+    if system().lower() == 'windows':
+        venv_path += '/Lib/site-packages'
+    else:
+        venv_path += '/lib/python3.11/site-packages'
+    locales = listdir('./locales')
     for locale in locales:
-        add_data.append(f'--add-data=locales/{locale};locales')
-    assets = listdir('assets')
+        add_data.append(f'--add-data=./locales/{locale};locales')
+    assets = listdir('./assets')
     for asset in assets:
         add_data.append(f'--add-data=assets/{asset};assets')
-    add_data.append(f'--add-data=.venv/Lib/site-packages/plyer;plyer')
-    add_data.append('--add-data=core/bot/handlers;core/bot/handlers')
+    add_data.append(f'--add-data={venv_path}/plyer;plyer')
+    add_data.append('--add-data=./core/bot/handlers;core/bot/handlers')
     if system() == 'Windows':
-        add_data.append('--add-data=.venv/Lib/site-packages/winsdk;winsdk')
+        add_data.append(f'--add-data={venv_path}/winsdk;winsdk')
         add_data.append('--hidden-import=winsdk')
-    add_data.append('--add-data=core/pc;core/pc')
+    add_data.append('--add-data=./core/pc;core/pc')
     add_data.append('--hidden-import=mss')
     add_data.append('--hidden-import=pynput')
     
-    PyInstaller.__main__.run([
+    all_args = [
         '--onefile',
         '--windowed',
         '--noconsole',
@@ -33,7 +38,11 @@ def main():
         '--name=TPC',
         *add_data,
         './main.py',
-    ])
+    ]
+    
+    if system().lower() == 'linux':
+        all_args = [i.replace(';', ':') for i in all_args]   
+    PyInstaller.__main__.run(all_args)
 
     
 if __name__ == '__main__':
