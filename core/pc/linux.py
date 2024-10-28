d703addc294821129d1a93e162bc2dd1375f43cb
nichind <nichinddev@gmail.com>
2024-10-28 23:39:28 +0300

    get playimg media, lock for Linux

--- core/pc/linux.py
+++ core/pc/linux.py
@@ -1,3 +1,6 @@
+import dbus 
+
+
 class PCHandlers:
     def __init__(self, tpc):
         self.tpc = tpc
@@ -7,3 +10,25 @@ class PCHandlers:
 
     async def on_shutdown(self):
         pass
+
+    async def get_playing_media(self):
+        bus = dbus.SessionBus()
+        for service in bus.list_names():
+            if service.startswith('org.mpris.MediaPlayer2.'):
+                media_player = bus.get_object(service, '/org/mpris/MediaPlayer2')
+                interface = dbus.Interface(media_player, dbus_interface='org.freedesktop.DBus.Properties')
+                metadata = interface.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
+                if metadata:
+                    info_dict = {
+                        'artist': metadata.get('xesam:artist')[0] if 'xesam:artist' in metadata else None,
+                        'title': metadata.get('xesam:title') if 'xesam:title' in metadata else None,
+                        'artUrl': metadata.get('mpris:artUrl') if 'mpris:artUrl' in metadata else None
+                    }
+                    return info_dict
+
+    async def lock(self):
+        # Lock the desktop using the dbus interface
+        bus = dbus.SessionBus()
+        obj = bus.get_object('org.gnome.ScreenSaver', '/org/gnome/ScreenSaver')
+        interface = dbus.Interface(obj, dbus_interface='org.gnome.ScreenSaver')
+        interface.Lock()
